807440a7333044de29673461bb7eff3e37dc233d
M. Musaab Siddiqui <65066022+mushi251@users.noreply.github.com>
2020-12-26 12:42:23 -0500

    Update extracting_features.sql
    Made some changes to formatting for visual

--- extracting_features.sql
+++ extracting_features.sql
@@ -7,33 +7,63 @@ DROP TABLE IF EXISTS FieldStats;
 DROP TABLE IF EXISTS HOF;
 DROP TABLE IF EXISTS output;
 
-#Batting Stats -Taking the sum of data from both The Batting and BattingPost Relational Instances
-CREATE TABLE Bat_Raw AS SELECT playerID, yearID, G, R, H, 2B, 3B, HR , SO from Batting UNION ALL SELECT  playerID, yearID, G, R, H, 2B, 3B, HR , SO from BattingPost;
-CREATE TABLE BatStats AS 
-SELECT playerID, count(distinct yearID) as Years_Batted , sum(G) as totGB, sum(R) as totRB, sum(H) as totHB, sum(2B) as tot2B, sum(3B) as tot3B, 
-sum(HR) as totHRB, sum(SO) as totSOB 
-FROM Bat_Raw GROUP BY playerID;
+-- Batting Stats -Taking the sum of data FROM both The Batting AND BattingPost Relational Instances
+CREATE TABLE Bat_Raw 
+AS SELECT playerID, yearID, G, R, H, 2B, 3B, HR , SO 
+FROM Batting 
+UNION ALL 
+SELECT  playerID, yearID, G, R, H, 2B, 3B, HR , SO 
+FROM BattingPost;
 
-#Pitching Stats - Taking the sum of data from both The Pitching and PitchingPost Relational Instances
-CREATE TABLE Pitch_Raw AS SELECT playerID, yearID, G, W, L, IPOuts, H , R, ER , BB , SO , HR from Pitching UNION ALL SELECT  playerID, yearID, G, W, L, IPOuts, H , R, ER , 
-BB , SO , HR from PitchingPost;
-CREATE TABLE PitchStats AS 
-SELECT playerID, count(distinct yearID) as Years_Pitched , sum(G) as totGP, sum(W) as totW, sum(L) as totL, sum(IPOuts) as totIPOuts, 
-sum(H) as totHP, sum(R) as totRP, sum(ER) as totER, sum(BB) as totBB, sum(SO) as totSOP, sum(HR) as totHRP 
-FROM Pitch_Raw GROUP BY playerID;
+CREATE TABLE BatStats 
+AS SELECT playerID, COUNT(DISTINCT yearID) AS Years_Batted , sum(G) AS totGB, sum(R) AS totRB, sum(H) AS totHB, sum(2B) AS tot2B, sum(3B) AS tot3B, 
+sum(HR) AS totHRB, sum(SO) AS totSOB 
+FROM Bat_Raw 
+GROUP BY playerID;
 
-#Fielding Stats - Taking the sum of data from both The Fielding and FieldingPost Relational Instances
-CREATE TABLE Field_Raw AS SELECT playerID, yearID, G, A, PO, E, DP, PB , CS, InnOuts, SB from Fielding UNION ALL SELECT  playerID, yearID, G, A, PO, E, DP, PB , CS, 
-InnOuts, SB from FieldingPost;
-CREATE TABLE FieldStats AS SELECT playerID, count(distinct yearID) as Years_Fielded , sum(G) as totGF, sum(A) as totA, sum(PO) as totPO, sum(E) as totE, sum(DP) 
-as totDP, sum(PB) as totPB, 
-sum(CS) as totCS, sum(InnOuts) as totInnOuts, sum(SB) as totSB FROM Field_Raw GROUP BY playerID;
+-- Pitching Stats - Taking the sum of data from both The Pitching and PitchingPost Relational Instances
+CREATE TABLE Pitch_Raw 
+AS SELECT playerID, yearID, G, W, L, IPOuts, H , R, ER , BB , SO , HR 
+FROM Pitching 
+UNION ALL 
+SELECT  playerID, yearID, G, W, L, IPOuts, H , R, ER , 
+BB , SO , HR 
+FROM PitchingPost;
 
-#HOF data - all that were nominated (inducted = Y or N) or elected (inducted = Y)
-CREATE TABLE HOF AS SELECT playerID, inducted from HallOfFame where (category = 'Player' and inducted = 'Y');
-#add the rest of the nominated who were not inducted
-INSERT INTO HOF (SELECT DISTINCT playerID, inducted from HallOfFame where (category = 'Player' AND playerID NOT IN (SELECT playerID from HallOfFame 
-where (category = 'Player' and inducted = 'Y'))));
+CREATE TABLE PitchStats 
+AS SELECT playerID, COUNT(DISTINCT yearID) AS Years_Pitched , sum(G) AS totGP, sum(W) ASs totW, sum(L) AS totL, sum(IPOuts) AS totIPOuts, sum(H) AS totHP, sum(R) AS totRP, 
+sum(ER) AS totER, sum(BB) AS totBB, sum(SO) AS totSOP, sum(HR) AS totHRP 
+FROM Pitch_Raw 
+GROUP BY playerID;
+
+-- Fielding Stats - Taking the sum of data FROM both The Fielding AND FieldingPost Relational Instances
+CREATE TABLE Field_Raw 
+AS SELECT playerID, yearID, G, A, PO, E, DP, PB , CS, InnOuts, SB 
+FROM Fielding 
+UNION ALL 
+SELECT  playerID, yearID, G, A, PO, E, DP, PB , CS, InnOuts, SB 
+FROM FieldingPost;
+
+CREATE TABLE FieldStats 
+AS SELECT playerID, COUNT(DISTINCT yearID) AS Years_Fielded , sum(G) AS totGF, sum(A) AS totA, sum(PO) AS totPO, sum(E) AS totE, sum(DP) 
+AS totDP, sum(PB) AS totPB, sum(CS) AS totCS, sum(InnOuts) AS totInnOuts, sum(SB) AS totSB 
+FROM Field_Raw 
+GROUP BY playerID;
+
+-- HOF data - all that were nominated (inducted = Y or N) or elected (inducted = Y)
+CREATE TABLE HOF 
+AS SELECT playerID, inducted 
+FROM HallOfFame 
+WHERE (category = 'Player' AND inducted = 'Y');
+-- add the rest of the nominated who were not inducted
+INSERT INTO HOF (SELECT DISTINCT playerID, inducted 
+                 FROM HallOfFame 
+                 WHERE (category = 'Player' 
+                        AND playerID 
+                        NOT IN (
+                          SELECT playerID 
+                          FROM HallOfFame 
+                          WHERE (category = 'Player' AND inducted = 'Y'))));
 
 -- Table for Task 1
 CREATE TABLE taska_out AS SELECT m.playerID, 
@@ -58,69 +88,69 @@ SET inducted = 1
 WHERE inducted = 'N';
 UPDATE taska_out 
 SET inducted = 0 
-WHERE inducted is null;
+WHERE inducted IS null;
 
 -- #2 Replacing any null values with 0. 
-update taska_out
-set totGB= 0 where totGB is null;
-update taska_out
-set totRB= 0 where totRB is null;
-update taska_out
-set totHB= 0 where totHB is null;
-update taska_out
-set tot2B= 0 where tot2B is null;
-update taska_out
-set tot3B= 0 where tot3B is null;
-update taska_out
-set totHRB= 0 where totHRB is null;
-update taska_out
-set totSOB= 0 where totSOB is null;
-update taska_out
-set Years_Pitched= 0 where Years_Pitched is null;
-update taska_out
-set totGP= 0 where totGP is null;
-update taska_out
-set totW= 0 where totW is null;
-update taska_out
-set totL= 0 where totL is null;
-update taska_out
-set totIPOuts= 0 where totIPOuts is null;
-update taska_out
-set totHP= 0 where totHP is null;
-update taska_out
-set totRP= 0 where totRP is null;
-update taska_out
-set totER= 0 where totER is null;
-update taska_out
-set totBB= 0 where totBB is null;
-update taska_out
-set totSOP= 0 where totSOP is null;
-update taska_out
-set totHRP= 0 where totHRP is null;
-update taska_out
-set totGF= 0 where totGF is null;
-update taska_out
-set totA= 0 where totA is null;
-update taska_out
-set totPO= 0 where totPO is null;
-update taska_out
-set totE= 0 where totE is null;
-update taska_out
-set totDP= 0 where totDP is null;
-update taska_out
-set totCS= 0 where totCS is null;
-update taska_out
-set totInnOuts= 0 where totInnOuts is null;
-update taska_out
-set totSB= 0 where totSB is null;
-update taska_out
-set Years_Fielded = 0 where Years_Fielded is null;
-update taska_out
-set debut= 0 where debut is null;
-update taska_out
-set finalGame= 0 where finalGame is null;
-update taska_out
-set deathYear= 0 where deathYear is null;
+UPDATE taska_out
+SET totGB= 0 WHERE totGB IS null;
+UPDATE taska_out
+SET totRB= 0 WHERE totRB IS null;
+UPDATE taska_out
+SET totHB= 0 WHERE totHB IS null;
+UPDATE taska_out
+SET tot2B= 0 WHERE tot2B IS null;
+UPDATE taska_out
+SET tot3B= 0 WHERE tot3B IS null;
+UPDATE taska_out
+SET totHRB= 0 WHERE totHRB IS null;
+UPDATE taska_out
+SET totSOB= 0 WHERE totSOB IS null;
+UPDATE taska_out
+SET Years_Pitched= 0 WHERE Years_Pitched IS null;
+UPDATE taska_out
+SET totGP= 0 WHERE totGP IS null;
+UPDATE taska_out
+SET totW= 0 WHERE totW IS null;
+UPDATE taska_out
+SET totL= 0 WHERE totL IS null;
+UPDATE taska_out
+SET totIPOuts= 0 WHERE totIPOuts IS null;
+UPDATE taska_out
+SET totHP= 0 WHERE totHP IS null;
+UPDATE taska_out
+SET totRP= 0 WHERE totRP IS null;
+UPDATE taska_out
+SET totER= 0 WHERE totER IS null;
+UPDATE taska_out
+SET totBB= 0 WHERE totBB IS null;
+UPDATE taska_out
+SET totSOP= 0 WHERE totSOP IS null;
+UPDATE taska_out
+SET totHRP= 0 WHERE totHRP IS null;
+UPDATE taska_out
+SET totGF= 0 WHERE totGF IS null;
+UPDATE taska_out
+SET totA= 0 WHERE totA IS null;
+UPDATE taska_out
+SET totPO= 0 WHERE totPO IS null;
+UPDATE taska_out
+SET totE= 0 WHERE totE IS null;
+UPDATE taska_out
+SET totDP= 0 WHERE totDP IS null;
+UPDATE taska_out
+SET totCS= 0 WHERE totCS IS null;
+UPDATE taska_out
+SET totInnOuts= 0 WHERE totInnOuts IS null;
+UPDATE taska_out
+SET totSB= 0 WHERE totSB IS null;
+UPDATE taska_out
+SET Years_Fielded = 0 WHERE Years_Fielded IS null;
+UPDATE taska_out
+SET debut= 0 WHERE debut IS null;
+UPDATE taska_out
+SET finalGame= 0 WHERE finalGame IS null;
+UPDATE taska_out
+SET deathYear= 0 WHERE deathYear IS null;
 
 -- Table for Task 2
 CREATE TABLE taskb_out AS SELECT h.playerID, 
@@ -151,67 +181,67 @@ WHERE inducted = 'N';
 
 -- #2 Replacing any null values with 0. 
 UPDATE taskb_out 
-set totGB= 0 where totGB is null;
+SET totGB= 0 WHERE totGB IS null;
 UPDATE taskb_out 
-set totRB= 0 where totRB is null;
+SET totRB= 0 WHERE totRB IS null;
 UPDATE taskb_out 
-set totHB= 0 where totHB is null;
+SET totHB= 0 WHERE totHB IS null;
 UPDATE taskb_out 
-set tot2B= 0 where tot2B is null;
+SET tot2B= 0 WHERE tot2B IS null;
 UPDATE taskb_out 
-set tot3B= 0 where tot3B is null;
+SET tot3B= 0 WHERE tot3B IS null;
 UPDATE taskb_out 
-set totHRB= 0 where totHRB is null;
+SET totHRB= 0 WHERE totHRB IS null;
 UPDATE taskb_out 
-set totSOB= 0 where totSOB is null;
+SET totSOB= 0 WHERE totSOB IS null;
 UPDATE taskb_out 
-set Years_Pitched= 0 where Years_Pitched is null;
+SET Years_Pitched= 0 WHERE Years_Pitched IS null;
 UPDATE taskb_out 
-set totGP= 0 where totGP is null;
+SET totGP= 0 WHERE totGP IS null;
 UPDATE taskb_out 
-set totW= 0 where totW is null;
+SET totW= 0 WHERE totW IS null;
 UPDATE taskb_out 
-set totL= 0 where totL is null;
+SET totL= 0 WHERE totL IS null;
 UPDATE taskb_out 
-set totIPOuts= 0 where totIPOuts is null;
+SET totIPOuts= 0 WHERE totIPOuts IS null;
 UPDATE taskb_out 
-set totHP= 0 where totHP is null;
+SET totHP= 0 WHERE totHP IS null;
 UPDATE taskb_out 
-set totRP= 0 where totRP is null;
+SET totRP= 0 WHERE totRP IS null;
 UPDATE taskb_out 
-set totER= 0 where totER is null;
+SET totER= 0 WHERE totER IS null;
 UPDATE taskb_out 
-set totBB= 0 where totBB is null;
+SET totBB= 0 WHERE totBB IS null;
 UPDATE taskb_out 
-set totSOP= 0 where totSOP is null;
+SET totSOP= 0 WHERE totSOP IS null;
 UPDATE taskb_out 
-set totHRP= 0 where totHRP is null;
+SET totHRP= 0 WHERE totHRP IS null;
 UPDATE taskb_out 
-set totGF= 0 where totGF is null;
+SET totGF= 0 WHERE totGF IS null;
 update taska_out
-set totA= 0 where totA is null;
+SET totA= 0 WHERE totA IS null;
 UPDATE taskb_out 
-set totPO= 0 where totPO is null;
+SET totPO= 0 WHERE totPO IS null;
 UPDATE taskb_out 
-set totE= 0 where totE is null;
+SET totE= 0 WHERE totE IS null;
 UPDATE taskb_out 
-set totDP= 0 where totDP is null;
+SET totDP= 0 WHERE totDP IS null;
 UPDATE taskb_out 
-set totCS= 0 where totCS is null;
+SET totCS= 0 WHERE totCS IS null;
 UPDATE taskb_out 
-set totInnOuts= 0 where totInnOuts is null;
+SET totInnOuts= 0 WHERE totInnOuts IS null;
 UPDATE taskb_out 
-set totSB= 0 where totSB is null;
+SET totSB= 0 WHERE totSB IS null;
 UPDATE taskb_out 
-set Years_Fielded = 0 where Years_Fielded is null;
+SET Years_Fielded = 0WHERE Years_Fielded IS null;
 UPDATE taskb_out 
-set debut= 0 where debut is null;
+SET debut= 0 WHERE debut IS null;
 UPDATE taskb_out 
-set finalGame= 0 where finalGame is null;
+SET finalGame= 0 WHERE finalGame IS null;
 UPDATE taskb_out 
-set deathYear= 0 where deathYear is null;
+SET deathYear= 0 WHERE deathYear IS null;
 
--- Extracting csv files for Task 1 and Task 2 respectively. Had to extract the csv code through virtual machine because of file priledge problems
+-- Extracting csv files for Task 1 and Task 2 respectively. Had to extract the csv code through virtual machine because of file priviledge problems
 -- For Task 1: mysql -B -h manta.uwaterloo.ca -u user_mm4siddi -p ece356db_mm4siddi -e "select * from taska_out" | sed 's/\t/,/g' > task1_extracted.csv
 -- For Task 2: mysql -B -h manta.uwaterloo.ca -u user_mm4siddi -p ece356db_mm4siddi -e "select * from taskb_out" | sed 's/\t/,/g' > task2_extracted.csv
 
